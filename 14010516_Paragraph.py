24816c54a2f9a2244641a5d5293a1759a52826f2
Marzban <adelmarzban@gmail.com>
2022-08-07 22:11:44 +0430

    -- Paragraph

---
+++ 14010516_Paragraph.py
@@ -0,0 +1,5 @@
+txt= "Python docx module allows user to manipulate docs by either manipulating the existing one or creating a new empty document and manipulating it. It is a powerful tool as it helps you to manipulate the document to a very large extend."
+
+print("Count of words= ", len(txt.split()))
+a=txt.count("Python"")
+print(a)
