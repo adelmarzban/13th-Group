6fa220866367aa224ae3c6de37485d8d9d329e94
Marzban <adelmarzban@gmail.com>
2022-08-08 01:38:44 +0430

    -- Paragraph2

--- 14010516_Paragraph.py
+++ 14010516_Paragraph.py
@@ -1,5 +1,21 @@
-txt= "Python docx module allows user to manipulate docs by either manipulating the existing one or creating a new empty document and manipulating it. It is a powerful tool as it helps you to manipulate the document to a very large extend."
+###txt is the paragraph we want to check###
+## txt is copined form a colleague python file##
+txt = """
+City life is full of fun. There are parks and picnic spots to visit. We have cinema halls to see movies.
+We have electricity which runs our factories, light and cools our home and helps us in seeing T.V.
+There are all type of amenities like water, health check up and transport.
+Sometimes circus shows and magic shows entertain the city people.
+"""
 
-print("Count of words= ", len(txt.split()))
-a=txt.count("Python"")
-print(a)
+###chtxt is created to make paragraph in lower case to be ready to check###
+chtxt = txt.lower()
+
+print("Count of words= ", len(chtxt.split()))
+
+toBe = [" am "," is "," are "," was "," were "]
+
+countToBe = 0
+for i in range (1,len(toBe)):
+    countToBe += chtxt.count(toBe[i])
+
+print(countToBe)
